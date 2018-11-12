8688ebe451b8a68241f8ee6fb555df24f782215a
team006 <wachirachai.006@mail.kmutt.ac.th>
2018-11-12 14:56:28 +0700

    [ADD] create project

---
+++ src/main/java/com/kamaab/softwarepro/SoftwareProApplication.java
@@ -0,0 +1,12 @@
+package com.kamaab.softwarepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareProApplication.class, args);
+    }
+}
