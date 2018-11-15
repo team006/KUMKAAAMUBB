734efc2a37755cf73db6c33c8c403f2859f06b02
SongwitBooddee <songwit.mik@mail.kmutt.ac.th>
2018-11-15 23:30:22 +0700

    [ADD]ProductRepository

---
+++ src/main/java/com/kamaab/softwarepro/Repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.kamaab.softwarepro.Repository;
+
+
+import com.kamaab.softwarepro.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}
