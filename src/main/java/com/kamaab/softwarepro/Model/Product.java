b796442cbbeb79550dee9bd28a8e76f4066c5765
SongwitBooddee <songwit.mik@mail.kmutt.ac.th>
2018-11-15 23:27:02 +0700

    [ADD] Product Model

---
+++ src/main/java/com/kamaab/softwarepro/Model/Product.java
@@ -0,0 +1,104 @@
+package com.kamaab.softwarepro.Model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name="products")
+
+public class Product implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long productId;
+
+    @NotBlank
+    private String productName;
+    @NotBlank
+    private double productPrice;
+
+    private int productAmount;
+
+    private String productSize;
+    @NotBlank
+    private String productImg;
+    @NotBlank
+    private String productType;
+
+    private String productDescription;
+
+    public Product(){
+
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductAmount() {
+        return productAmount;
+    }
+
+    public void setProductAmount(int productAmount) {
+        this.productAmount = productAmount;
+    }
+
+    public String getProductSize() {
+        return productSize;
+    }
+
+    public void setProductSize(String productSize) {
+        this.productSize = productSize;
+    }
+
+    public String getProductImg() {
+        return productImg;
+    }
+
+    public void setProductImg(String productImg) {
+        this.productImg = productImg;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+}
