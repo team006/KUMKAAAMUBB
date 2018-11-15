f0238004da5081f0b9c54333e7795cd72a382f9a
team006 <wachirachai.006@mail.kmutt.ac.th>
2018-11-15 16:08:24 +0700

    [ADD] Facebook Login

--- src/main/java/com/kamaab/softwarepro/SoftwareProApplication.java
+++ src/main/java/com/kamaab/softwarepro/SoftwareProApplication.java
@@ -2,11 +2,38 @@ package com.kamaab.softwarepro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class SoftwareProApplication {
+@EnableOAuth2Sso
+@RestController
+public class SoftwareProApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(SoftwareProApplication.class, args);
+    }
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());;
     }
 }
