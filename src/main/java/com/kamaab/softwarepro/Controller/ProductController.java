dd77b3e32970a290cd3d2c21eec7eca2c278776e
SongwitBooddee <songwit.mik@mail.kmutt.ac.th>
2018-11-19 16:45:55 +0700

    [EDIT]Frontend

--- src/main/java/com/kamaab/softwarepro/Controller/ProductController.java
+++ src/main/java/com/kamaab/softwarepro/Controller/ProductController.java
@@ -7,17 +7,16 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 
-@RestController
+@Controller
 class ProductController {
 
     @Autowired
